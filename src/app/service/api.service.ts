7da6580506d69746795de8c4b633a9981d0f3cc2
Shu Han <codershuhan@gmail.com>
2021-11-14 18:30:52 -0800

    update url

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -8,8 +8,8 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  private apiRoute = "https://localhost:5001/iot";
-  // private apiRoute = "https://iot-cloud-server.azurewebsites.net/iot";
+  // private apiRoute = "https://localhost:5001/iot";
+  private apiRoute = "https://iot-cloud-server.azurewebsites.net/iot";
 
   public uploadFileToBlob(location: string, fileToUpload: FormData){
     return this.http.post<any>(`${this.apiRoute}/UploadFileToBlob/${location}`, fileToUpload, {reportProgress: true, observe: 'events'});
